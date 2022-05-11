79608e8af341f329f4e40a6c0023044b1992571b
michael-nevius <michael.a.nevius@gmail.com>
2022-05-11 10:33:40 -0400

    added search by likes

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.music_library),
-    path('<int:pk>/', views.music_detail)
+    path('<int:pk>/', views.music_detail),
+    path('<int:pk>/like/', views.like_song),
 ]
